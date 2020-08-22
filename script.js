325c155eef6e3a37996454de7fa5f09125716f49
Yerson Arvelo <yerson.a06@gmail.com>
2020-08-22 03:19:39 -0400

    completed and tested prompt for length

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+// Write password to the #password input
+//function start() {
+    //console.log("we're here!");
+    //var password = generatePassword();
+    //var passwordText = document.querySelector("#password");
+    //passwordText.value = password;
+ //}
 
-  //vars definition
-  var length;
-  var lowercase = false;
-  var uppercase = false;
-  var numeric = false;
-  var specialCharacters = false;
+// return a string
+function generatePassword() {
 
   // array of uppercase letters
   var upperCaseArray = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",] 
@@ -18,6 +20,21 @@ function generatePassword() {
   var specialCharactersArray = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",];
   // array of numbers
   var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  
+
+  // ====== DEFINING FUNCTIONS ====== 
+  
+  // get user options and store in vars
+  function getUserOptions() {
+    var passwordLength = prompt("Number of characters in password? Should be longer than 8 and shorter than 128!");
+    if ((passwordLength < "8") || (passwordLength > "128")) {
+        alert("must have password between 8 and 128 characters")
+    }
+    var confirmLowerCase = confirm("Include lowercase characters?");
+    var confirmUpperCase = confirm("Include uppercase characters?");
+    var confirmNumeric = confirm("Include numeric values?");
+    var confirmSpecialCharacters = confirm("Include special characters?");
+  }
   
   //prompt for length
   
